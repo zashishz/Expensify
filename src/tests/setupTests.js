7738005a65d4763bcfecb45730c8b91794cc3df2
ashish.g.verma <ashish.g.verma@accenture.com>
2017-12-12 14:22:14 +0530

    Added Env Variables for Test and Dev

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,5 +1,8 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test'});
 
 Enzyme.configure({
     adapter: new Adapter()
